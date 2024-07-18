4586cbc573d8ac2cac46f1980bb07c29e8d2dc8b
HEADYKAGE <105305487+HEADYKAGE@users.noreply.github.com>
2024-07-19 02:29:34 +0530

    working slider changes Cards

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // alert("Error");
 
 // document.querySelector('video').playbackRate = 10.0;
+src="https://unpkg.com/scrollreveal"
+
+ScrollReveal({ reset: true });
+    
+    
+ScrollReveal.reveal(".hero");
